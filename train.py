0dbe749f133ca1e90118a29ba7f1c86bde3f6af5
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-08-28 21:55:09 -0400

    Create initial structure

---
+++ train.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Trains, evaluates and saves the TensorDetect model."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import logging
+import os
+import sys
+
+# configure logging
+if 'TV_IS_DEV' in os.environ and os.environ['TV_IS_DEV']:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.INFO,
+                        stream=sys.stdout)
+else:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.INFO,
+                        stream=sys.stdout)
+
+# https://github.com/tensorflow/tensorflow/issues/2034#issuecomment-220820070
+import numpy as np
+import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+sys.path.insert(1, 'incl')
+
+import tensorvision.train as train
+import tensorvision.utils as utils
+
+flags.DEFINE_string('name', None,
+                    'Append a name Tag to run.')
+
+flags.DEFINE_string('project', None,
+                    'Append a name Tag to run.')
+
+flags.DEFINE_string('hypes', 'hypes\kitti_fcn.json',
+                    'File storing model parameters.')
+
+tf.app.flags.DEFINE_boolean(
+    'save', True, ('Whether to save the run. In case --nosave (default) '
+                   'output will be saved to the folder TV_DIR_RUNS/debug, '
+                   'hence it will get overwritten by further runs.'))
+
+
+def main(_):
+    utils.set_gpus_to_use()
+
+    with open(tf.app.flags.FLAGS.hypes, 'r') as f:
+        logging.info("f: %s", f)
+        hypes = json.load(f)
+    utils.load_plugins()
+
+    if 'TV_DIR_RUNS' in os.environ:
+        os.environ['TV_DIR_RUNS'] = os.path.join(os.environ['TV_DIR_RUNS'],
+                                                 'TensorDetect2')
+    utils.set_dirs(hypes, tf.app.flags.FLAGS.hypes)
+
+    utils._add_paths_to_sys(hypes)
+
+    logging.info("Initialize training folder")
+    train.initialize_training_folder(hypes)
+    train.maybe_download_and_extract(hypes)
+    logging.info("Start training")
+    train.do_training(hypes)
+
+
+if __name__ == '__main__':
+    tf.app.run()
