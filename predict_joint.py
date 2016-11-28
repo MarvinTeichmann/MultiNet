efa892b8b2b37a07a8d39237b8e96bf1c71c8dec
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-11-28 16:49:39 +0000

    Update joint prediction script.
    Perform joint inference and plot results when calling predict_joint.py

--- predict_joint.py
+++ predict_joint.py
@@ -40,15 +40,18 @@ import train as united_train
 import tensorvision.train as train
 import tensorvision.utils as utils
 import tensorvision.core as core
+from PIL import Image, ImageDraw, ImageFont
 
-my_logdir = ('/home/mifs/mttt2/local_disk/RUNS/UnitedVision2/'
-             'new_code_test/road_no_fcn_multinet3_2016_11_06_21.02')
+my_logdir = ('/home/mifs/mttt2/local_disk/rsync/Backup/'
+             'RUNS/paper/multinet3')
 data_file = "/scratch/marvin/DATA/data_road/testing.txt"
 
-output_folder = "/home/mifs/mttt2/local_disk/RUNS/results"
-run_folder = 'tracking'
+res_folder = 'results'
 
-output_folder = os.path.join(output_folder, run_folder)
+output_folder = os.path.join(my_logdir, res_folder)
+
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
 
 
 def _output_generator(sess, tensor_list, image_pl, data_file,
@@ -61,6 +64,7 @@ def _output_generator(sess, tensor_list, image_pl, data_file,
             image_file = os.path.join(image_dir, image_file)
 
             image = scp.misc.imread(image_file)
+
             image = process_image(image)
 
             feed_dict = {image_pl: image}
@@ -70,6 +74,8 @@ def _output_generator(sess, tensor_list, image_pl, data_file,
 
 
 def eval_runtime(sess, subhypes, image_pl, eval_list, data_file):
+    logging.info(' ')
+    logging.info('Evaluation complete. Measuring runtime.')
     image_dir = os.path.dirname(data_file)
     with open(data_file) as file:
         for datum in file:
@@ -85,9 +91,78 @@ def eval_runtime(sess, subhypes, image_pl, eval_list, data_file):
     for i in xrange(100):
         _ = sess.run(eval_list, feed_dict=feed)
     dt = (time.time() - start_time)/100
-    logging.info('Speed (msec) %f: ', 1000*dt)
-    logging.info('Speed (fps) %f: ', 1/dt)
+    logging.info('Joined inference can be conducted at the following rates on'
+                 ' your machine:')
+    logging.info('Speed (msec): %f ', 1000*dt)
+    logging.info('Speed (fps): %f ', 1/dt)
     return dt
+
+
+def test_constant_input(subhypes):
+    road_input_conf = subhypes['road']['jitter']
+    seg_input_conf = subhypes['segmentation']['jitter']
+    car_input_conf = subhypes['detection']
+
+    gesund = True \
+        and road_input_conf['image_width'] == seg_input_conf['image_width'] \
+        and road_input_conf['image_height'] == seg_input_conf['image_height'] \
+        and car_input_conf['image_width'] == seg_input_conf['image_width'] \
+        and car_input_conf['image_height'] == seg_input_conf['image_height'] \
+
+    if not gesund:
+        logging.error("The different tasks are training"
+                      "using different resolutions. Please retrain all tasks,"
+                      "using the same resolution.")
+        exit(1)
+    return
+
+
+def test_segmentation_input(subhypes):
+
+    if not subhypes['segmentation']['jitter']['reseize_image']:
+        logging.error('')
+        logging.error("Issue with Segmentation input handling.")
+        logging.error("Segmentation input will be resized during this"
+                      "evaluation, but was not resized during training.")
+        logging.error("This will lead to bad results.")
+        logging.error("To use this script please train segmentation using"
+                      "the configuration:.")
+        logging.error("""
+{
+    "jitter": {
+    "reseize_image": true,
+    "image_height" : 384,
+    "image_width" : 1248,
+    },
+}""")
+        logging.error("Alternatively implement evaluation using non-resized"
+                      " input.")
+        exit(1)
+    return
+
+
+def road_draw(image, highway):
+    im = Image.fromarray(image.astype('uint8'))
+    draw = ImageDraw.Draw(im)
+
+    fnt = ImageFont.truetype('FreeMono/FreeMonoBold.ttf', 40)
+
+    shape = image.shape
+
+    if highway:
+        draw.text((65, 10), "Highway",
+                  font=fnt, fill=(255, 255, 0, 255))
+
+        draw.ellipse([10, 10, 55, 55], fill=(255, 255, 0, 255),
+                     outline=(255, 255, 0, 255))
+    else:
+        draw.text((65, 10), "minor road",
+                  font=fnt, fill=(255, 0, 0, 255))
+
+        draw.ellipse([10, 10, 55, 55], fill=(255, 0, 0, 255),
+                     outline=(255, 0, 0, 255))
+
+    return np.array(im).astype('float32')
 
 
 def run_eval(load_out):
@@ -100,23 +175,49 @@ def run_eval(load_out):
     road_softmax = decoded_logits['road']['softmax'][0]
     eval_list = [seg_softmax, pred_boxes_new, pred_confidences, road_softmax]
 
-    eval_runtime(sess, subhypes, image_pl, eval_list, data_file)
-    exit(0)
-
     def my_process(image):
-        process_image(subhypes, image)
+        return process_image(subhypes, image)
+
+    test_constant_input(subhypes)
+    test_segmentation_input(subhypes)
+
+    import utils.train_utils as dec_utils
 
     gen = _output_generator(sess, eval_list, image_pl, data_file, my_process)
     for image_file, output in gen:
         image = scp.misc.imread(image_file)
+        image = process_image(subhypes, image)
         shape = image.shape
-        output = output[:, 1].reshape(shape[0], shape[1])
-        hard = output > 0.5
+        seg_softmax, pred_boxes_new, pred_confidences, road_softmax = output
+
+        # Create Segmentation Overlay
+        shape = image.shape
+        seg_softmax = seg_softmax[:, 1].reshape(shape[0], shape[1])
+        hard = seg_softmax > 0.5
         overlay_image = utils.fast_overlay(image, hard)
+
+        # Draw Detection Boxes
+        new_img, rects = dec_utils.add_rectangles(
+            subhypes['detection'], [overlay_image], pred_confidences,
+            pred_boxes_new, show_removed=False,
+            use_stitching=True, rnn_len=subhypes['detection']['rnn_len'],
+            min_conf=0.5, tau=0.4)
+
+        # Draw road classification
+        highway = (np.argmax(output[0][0]) == 0)
+        new_img = road_draw(new_img, highway)
+
+        # Save image file
         im_name = os.path.basename(image_file)
         new_im_file = os.path.join(output_folder, im_name)
-        scp.misc.imsave(new_im_file, overlay_image)
-        print(image_file)
+        im_name = os.path.basename(image_file)
+        new_im_file = os.path.join(output_folder, im_name)
+        scp.misc.imsave(new_im_file, new_img)
+
+        logging.info("Plotting file: {}".format(new_im_file))
+
+    eval_runtime(sess, subhypes, image_pl, eval_list, data_file)
+    exit(0)
 
 
 def process_image(subhypes, image):
@@ -127,12 +228,9 @@ def process_image(subhypes, image):
     assert(image_height >= shape[0])
     assert(image_width >= shape[1])
 
-    offset_x = (image_height - shape[0])//2
-    offset_y = (image_width - shape[1])//2
-    new_image = np.zeros([image_height, image_width, 3])
-    new_image[offset_x:offset_x+shape[0],
-              offset_y:offset_y+shape[1]] = image
-    input_image = new_image
+    image = scp.misc.imresize(image, (image_height,
+                                      image_width, 3),
+                              interp='cubic')
     return image
 
 
