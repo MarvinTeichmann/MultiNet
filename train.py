5f1b382671d4c1d6d7ffd98e2a2a74a5ca352023
Marvin Teichmann <marvin.teichmann@googlemail.com>
2017-02-19 17:42:41 +0000

    Update MultiNet to be compatible with new versions

--- train.py
+++ train.py
@@ -149,7 +149,7 @@ def build_training_graph(hypes, queue, modules, first_iter):
         eval_list = objective.evaluation(
             hypes, image, labels, decoded_logits, losses, global_step)
 
-        summary_op = tf.merge_all_summaries()
+        summary_op = tf.summary.merge_all()
 
     graph = {}
     graph['losses'] = losses
@@ -589,7 +589,7 @@ def main(_):
 
     if 'TV_DIR_RUNS' in os.environ:
         os.environ['TV_DIR_RUNS'] = os.path.join(os.environ['TV_DIR_RUNS'],
-                                                 'UnitedVision2')
+                                                 'MultiNet')
 
     if not load_weights:
         utils.set_dirs(hypes, tf.app.flags.FLAGS.hypes)
