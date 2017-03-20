5ba41b09715f18785da53441666dca3c4b93b14a
Marvin Teichmann <marvin.teichmann@googlemail.com>
2017-03-20 17:50:55 +0000

    Set base_path correctly.
    Fixes #6.

--- demo.py
+++ demo.py
@@ -242,7 +242,8 @@ def load_united_model(logdir):
 
     first_iter = True
 
-    meta_hypes = tv_utils.load_hypes_from_logdir(logdir, subdir="")
+    meta_hypes = tv_utils.load_hypes_from_logdir(logdir, subdir="",
+                                                 base_path='hypes')
     for model in meta_hypes['models']:
         subhypes[model] = tv_utils.load_hypes_from_logdir(logdir, subdir=model)
         hypes = subhypes[model]
